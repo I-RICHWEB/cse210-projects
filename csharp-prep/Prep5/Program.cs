a027b6ad7e4d08cd71cac43957e0835cfe83c594
I-RICHWEB <ize20002@byui.edu>
2023-01-12 03:56:26 +0100

    prep 5 assiggnment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,33 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
-    }
+        static void Greetings()
+        {
+            Console.WriteLine("Welcome to the Program.");
+        }
+        Greetings();
+        static string Username()
+        {
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            return name;
+        }
+        string name = Username();
+        static int Number()
+        {
+            Console.Write("What is your favorite number?  ");
+            string nums = Console.ReadLine();
+            int num = int.Parse(nums);
+            return num;
+        }
+        int squu = Number();
+        static int Sqare(int para)
+        {
+            int squ = para * para;
+            return squ;
+        }
+        int sq = Sqare(squu);
+        Console.WriteLine($"{name}, the square of your number is: {sq} ");
+
+    }        
 }
