348b4cc86af13e7e509655b6ce2631f94ab1ebb7
I-RICHWEB <ize20002@byui.edu>
2023-01-21 14:45:29 +0100

    prepare assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2018;
+        job1._endYear = 2021;
+        job1.JobDisplay();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2021;
+        job2._endYear = 2022;
+        job2.JobDisplay();
+
+        Resume myResume = new Resume();
+        myResume._name = "Richmond Uyi";
+        myResume._jobList.Add(job1);
+        myResume._jobList.Add(job2);
+        myResume.Display();
+
     }
 }
